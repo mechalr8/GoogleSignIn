8ce5250afae10d67d6c552dc0cfa0ffc76e71b66
mechalr8 <harsitagrawal59@gmail.com>
2023-02-09 13:39:25 +0530

    Updated oauth credentials

--- app/src/main/java/com/example/googleoauth20signin/MainActivity.kt
+++ app/src/main/java/com/example/googleoauth20signin/MainActivity.kt
@@ -8,14 +8,12 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.os.BuildCompat
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 //import com.google.android.gms.common.SignInButton
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
-import java.security.KeyStore.Builder
 
 
 class MainActivity : AppCompatActivity() {
